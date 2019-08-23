766aafa66db9794b9e852ae1f7dd4e4f159a1eaf
Maeve Griffin <griffinmaeve@gmail.com>
2019-08-23 15:22:04 -0700

    file structure, html and css

---
+++ public/src/data/milerals.js
@@ -0,0 +1,44 @@
+const minerals = [
+    {
+        name: 'quarts',
+        clear: true,
+        color: 'clear',
+        density: 2.62,
+        type: 'trigonal-trapezohedral',
+        imgSrc: './src/assets/quartz.jpg'
+    },
+    {
+        name: 'gold',
+        clear: false,
+        color: 'yellow',
+        density: 17.64,
+        type: 'isometric-hexoctahedral',
+        imgSrc: './src/assets/gold.jpg'
+    },
+    {
+        name: 'fluorite',
+        clear: true,
+        color: 'teal or orange',
+        density: 3.13,
+        type: 'isometric-hexoctahedral',
+        imgSrc: './src/assets/fluorite.jpg'
+    },
+    {
+        name: 'graphite',
+        clear: false,
+        color: 'dark grey',
+        density: 3.4,
+        type: 'isometric-diploidal',
+        imgSrc: './src/assets/graphite.jpg'
+    },
+    {
+        name: 'pyrite',
+        clear: false,
+        color: 'pale yellow',
+        density: 5.1,
+        type: 'isometric-diploidal',
+        imgSrc: './src/assets/pyrite.jpg'
+    },
+];
+
+export default minerals;
